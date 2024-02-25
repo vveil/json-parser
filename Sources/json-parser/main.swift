c81ddbec269d569f06ec05aa52a4212dc7d06224
Niklas <account@niklas.ai>
2024-02-25 19:03:36 +0100

    saving commit, currently broken

--- Sources/json-parser/main.swift
+++ Sources/json-parser/main.swift
@@ -1,3 +1,11 @@
 import Foundation
 
 var arguments = CommandLine.arguments
+
+print(arguments)
+
+let filePath = Array(arguments[1...]).filter({ !$0.hasPrefix("-") }).first {
+var content = ""
+if let fileContent = try? String(contentsOfFile: path) {
+  content = fileContent
+}
