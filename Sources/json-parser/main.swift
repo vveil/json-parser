73f338dad8b0f8ac4225b7f9c4fbfb132cdcd9ab
Niklas <account@niklas.ai>
2024-02-27 16:39:43 +0100

    small improvemnt

--- Sources/json-parser/main.swift
+++ Sources/json-parser/main.swift
@@ -9,5 +9,10 @@ if let filePath = Array(arguments[1...]).filter({ !$0.hasPrefix("-") }).first {
   if let fileContent = try? String(contentsOfFile: filePath) {
     content = fileContent
   }
-  print(content)
+  if content == "{}" {
+    print("valid empty json")
+    exit(0)
+  }
+  print("invalid json")
+  exit(1)
 }
