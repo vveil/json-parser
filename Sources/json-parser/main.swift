273c3737c603ae8c0fdf7e5f4b3d051a682783da
Niklas <account@niklas.ai>
2024-02-28 19:16:14 +0100

    parsing content into lines

--- Sources/json-parser/main.swift
+++ Sources/json-parser/main.swift
@@ -9,6 +9,11 @@ if let filePath = Array(arguments[1...]).filter({ !$0.hasPrefix("-") }).first {
   if let fileContent = try? String(contentsOfFile: filePath) {
     content = fileContent
   }
+  var contentLines = content.split(separator: "\n")
+  if contentLines.count == 1 {
+    contentLines = content.split(separator: " ")
+  }
+  print(contentLines)
   if content == "{}" {
     print("valid empty json")
     exit(0)
