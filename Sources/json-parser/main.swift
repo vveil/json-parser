38bb8be7ec12f2e1b70ef9fbd959aaeba4b399d6
Niklas <account@niklas.ai>
2024-02-29 21:01:43 +0100

    small improvement

--- Sources/json-parser/main.swift
+++ Sources/json-parser/main.swift
@@ -9,15 +9,21 @@ if let filePath = Array(arguments[1...]).filter({ !$0.hasPrefix("-") }).first {
   if let fileContent = try? String(contentsOfFile: filePath) {
     content = fileContent
   }
+  if content == "{}" {
+    print("valid empty json")
+    exit(0)
+  }
+
   var contentLines = content.split(separator: "\n")
   if contentLines.count == 1 {
     contentLines = content.split(separator: " ")
   }
   print(contentLines)
-  if content == "{}" {
-    print("valid empty json")
-    exit(0)
+  if content.replacingOccurrences(of: "\\s+", with: "", options: .regularExpression).contains(",}")
+  {
+    print("invalid symbol in front of closing bracket")
   }
+
   print("invalid json")
   exit(1)
 }
