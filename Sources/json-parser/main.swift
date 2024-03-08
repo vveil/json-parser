63b7e7f00d7134fdb705df16dde779343ffbb474
Niklas <account@niklas.ai>
2024-03-08 16:31:22 +0100

    trying another approach, starting from scratch

--- Sources/json-parser/main.swift
+++ Sources/json-parser/main.swift
@@ -1,6 +1,39 @@
 import Foundation
 import RegexBuilder
 
+enum JSONState {
+  case key
+  case value
+  case object
+  case string
+  case nothing
+}
+
+class JSONValidator {
+  var stateStack: [JSONState] = [.nothing]
+  var state: JSONState = .nothing
+  var error: [String] = []
+  func validate(_ content: String) -> [String] {
+    for char in content {
+      switch char {
+      case "{":
+        stateStack.append(.object)
+      case "}":
+        stateStack.removeLast()
+        if stateStack == [.nothing] {
+          stateStack = []
+        }
+      default:
+        break
+      }
+    }
+    if !stateStack.isEmpty {
+      error.append("Invalid JSON")
+    }
+    return error
+  }
+}
+
 var arguments = CommandLine.arguments
 
 print(arguments)
@@ -10,58 +43,12 @@ if let filePath = Array(arguments[1...]).filter({ !$0.hasPrefix("-") }).first {
   if let fileContent = try? String(contentsOfFile: filePath) {
     content = fileContent
   }
-  if content == "{}" {
-    print("valid empty json")
-    exit(0)
-  }
-  // TODO complete regex
-  let regex = Regex {
-    "{"
-    ZeroOrMore {
-      ZeroOrMore("\n")
-      "\""
-      OneOrMore(.word)
-      "\""
-      ZeroOrMore(.whitespace)
-      ":"
-      ZeroOrMore(.whitespace)
-      "\""
-      OneOrMore(.word)
-      "\""
-      // ZeroOrMore(.any)
-      // ZeroOrMore("\n")
-    }
-    // ZeroOrMore("\n")
-    "}"
-  }
-  print(content)
-  if let matches = content.wholeMatch(of: regex) {
-    // print(matches)
-    print("whole match")
-    exit(0)
+
+  let error = JSONValidator().validate(content)
+  if error.isEmpty {
+    print("Valid JSON")
+  } else {
+    print(error.joined(separator: "\n"))
   }
 
-  if let match = try? regex.firstMatch(in: content) {
-    print("first match")
-    print("match: \(match.0)")
-    exit(0)
-  }
-
-  var contentLines = content.split(separator: "\n")
-  if contentLines.count == 1 {
-    contentLines = content.split(separator: " ")
-  }
-  print(contentLines)
-  let contentWithoutNewlinesAndWhitespaces = content.replacingOccurrences(
-    of: "\\s+", with: "", options: .regularExpression)
-
-  if contentWithoutNewlinesAndWhitespaces.contains("{,") {
-    print("invalid symbol in front of opening bracket")
-  }
-  if contentWithoutNewlinesAndWhitespaces.contains(",}") {
-    print("invalid symbol in front of closing bracket")
-  }
-
-  print("invalid json")
-  exit(1)
 }
